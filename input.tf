3e34024a22f6ae9c592a4e35968f137b4652d3ab
Amudha Palani <53053396+AmudaPalani@users.noreply.github.com>
2023-08-02 14:16:15 -0500

    Create input.tf

---
+++ input.tf
@@ -0,0 +1,35 @@
+variable "tenant_id" {
+  description = "tenant id"
+  default     = ""
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "subscription id"
+  default     = ""
+  type        = string
+}
+
+variable "client_id" {
+  description = "client id"
+  default     = ""
+  type        = string
+}
+
+variable "client_secret" {
+  description = "client secret"
+  default     = ""
+  type        = string
+}
+
+variable "rg_name" {
+  default     = "falcon"
+  type        = string
+  description = "Name for rg"
+}
+variable "location" {
+  default     = "westus2"
+  type        = string
+  description = "Location of Azure resources"
+}
+
