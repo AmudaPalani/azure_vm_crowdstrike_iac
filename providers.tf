6a96b45bd834d66af63f1160a580096a2b4bb8a9
Amudha Palani <53053396+AmudaPalani@users.noreply.github.com>
2023-08-02 14:30:56 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,13 @@
-data "azurerm_subscription" "current" {
-  subscription_id = var.subscription_id
-}
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "=3.25.0"
+        }
+    }
+}  
 
 provider "azurerm" {
-  #Pin the version
-  #See https://github.com/terraform-providers/terraform-provider-azurerm/blob/master/CHANGELOG.md for latest version
-  version = ">=3.20.0"
-
   tenant_id                  = var.tenant_id
   subscription_id            = var.subscription_id
   client_id                  = var.client_id
